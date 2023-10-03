d3795eeea19d5bf094da6ae217f2eb7339e7c6eb
Your Name <you@example.com>
2023-10-03 16:22:12 +0100

    HW

--- countVowels.py
+++ countVowels.py
@@ -1,14 +1,26 @@
+#word = input("Please enter a word: ")
+#counter = 0
+
+#if word.find('a'):
+    #counter += 1
+#elif word.find('e'):
+    #counter += 1
+#elif word.find('i'):
+    #counter += 1
+#elif word.find('o'):
+    #counter += 1
+#elif word.find('u'):
+    #counter += 1
+#print("Your word is ", len(word), "letters long.")
+#print ("Your word has", counter, "vowels.")
+
 word = input("Please enter a word: ")
 counter = 0
-print(len(word))
-if word.find('a'):
+vowels = ['a', 'e' 'i', 'o', 'u']
+vcount = 0
+
+while counter < len(word):
+    if word[counter] in vowels:
+        vcount += 1
     counter += 1
-elif word.find('e'):
-    counter += 1
-elif word.find('i'):
-    counter += 1
-elif word.find('o'):
-    counter += 1
-elif word.find('u'):
-    counter += 1
-print ("Your word has", counter, "vowels")
+print("Your word has ", vcount, "vowels")
---
+++ timeCalculator.py
@@ -0,0 +1,4 @@
+time1 = input("Please enter a day-time string (Format DD:HH:MM : ")
+time2 = input("Please enter another day-time string (Format DD:HH:MM : ")
+menu = "Time Calculator \n 1.Add 2 times \n 2.Find the difference between 2 times \n 3.Convert to Hours  \n 4.Convert to Minutes \n 5.Convert Minutes to Time \n 6.Convert Hours to Time \n 7.Convert Days to Time \n 8.Exit \n Enter an option: " 
+print(menu)
